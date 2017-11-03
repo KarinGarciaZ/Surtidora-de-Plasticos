84fa7c6a88fe5e1899a1e8ff5d8d450c2bd4a5c4
KarinGarciaZ <oscarin962011@hotmail.com>
2017-11-02 21:10:06 -0600

    First aproach.

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { environment } from './../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule } from'@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FoooterComponent } from './foooter/foooter.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { LoginComponent } from './login/login.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin-orders/admin-orders.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FoooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    CategoriesComponent,
+    LoginComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin/products', component: AdminProductsComponent },
+      { path: 'admin/orders', component: AdminOrdersComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBy3_TwYu3yIigr-UNbJJAi6EK0HxyS8oc",
+    authDomain: "surtidoradeplasticos-6b593.firebaseapp.com",
+    databaseURL: "https://surtidoradeplasticos-6b593.firebaseio.com",
+    projectId: "surtidoradeplasticos-6b593",
+    storageBucket: "surtidoradeplasticos-6b593.appspot.com",
+    messagingSenderId: "98797048210"
+  }
+};
