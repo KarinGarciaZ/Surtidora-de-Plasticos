ea1fe9c55387b30f8df5da30630565c0e79cf9c1
KarinGarciaZ <oscarin962011@hotmail.com>
2017-11-16 20:06:17 -0600

    Added the data-table and implemented.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { Product } from './../../models/product';
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import { DataTableResource } from 'angular-4-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -12,10 +13,16 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: any[];
   subscription: Subscription;
+  tableResourse: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
 
   constructor( private productService: ProductService ) {
     this.subscription = this.productService.getAll()
-    .subscribe( products => this.filteredProducts = this.products = products );
+    .subscribe( products => {
+      this.filteredProducts = this.products = products;
+      this.initializeTable( products );      
+    });
    }
 
   ngOnInit() {
@@ -29,6 +36,21 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
     this.filteredProducts = ( query ) ?
       this.products.filter( p => p.title.toLowerCase().includes(query.toLowerCase())) :
       this.products;
+  }
+
+  reloadItems( event ) {
+    if ( !this.tableResourse ) return false;
+
+    this.tableResourse.query(event)
+    .then( items => this.items = items );
+  }
+
+  initializeTable( products: Product[] ) {
+    this.tableResourse = new DataTableResource(products);
+    this.tableResourse.query({ offset: 0 })
+      .then( items => this.items = items );
+    this.tableResourse.count()
+      .then( count => this.itemCount = count );
   }
 
 }
